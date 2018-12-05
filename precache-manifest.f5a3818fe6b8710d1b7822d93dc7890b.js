60a59437a52569b1819f5fee5c6922ec27a582b3
Hang Cui <oscarcuihang@hotmail.com>
2018-12-05 12:44:47 -0500

    Updates

---
+++ precache-manifest.f5a3818fe6b8710d1b7822d93dc7890b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4ad292dd6fd87829b262",
+    "url": "/resume.io/static/js/runtime~main.4ad292dd.js"
+  },
+  {
+    "revision": "7f25e904d23bc75b7a95",
+    "url": "/resume.io/static/js/main.7f25e904.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/resume.io/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "7f25e904d23bc75b7a95",
+    "url": "/resume.io/static/css/main.2ef4c723.chunk.css"
+  },
+  {
+    "revision": "b929b1a4590ba411f44384ad0796ee07",
+    "url": "/resume.io/index.html"
+  }
+];
