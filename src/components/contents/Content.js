e32c52670624fb26ca66671d7246a46471333f7b
Hang Cui <oscarcuihang@hotmail.com>
2018-12-21 00:09:20 -0500

    Create Edu Proj Components (#2)

--- src/components/contents/Content.js
+++ src/components/contents/Content.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { Layout } from 'antd';
 import PropTypes from 'prop-types'
 
-import Education from './Educations'
-import Projects from './Projects'
-import Experiences from './Experiences'
+import Education from './Education'
+import Projects from './Project'
+import Experiences from './Experience'
 
 const { Content } = Layout;
 
@@ -13,12 +13,34 @@ class AppContent extends Component {
     myData: PropTypes.object.isRequired,
   }
 
+  renderEducationSection() {
+    if (this.props.myData.educations.length < 1) {
+      return null;
+    }
+    return 'Educations';
+  }
+
+  renderProjectSection() {
+    if (this.props.myData.otherProjects.length < 1) {
+      return null;
+    }
+    return 'Projects';
+
+  }
+
+  renderExperienceSection() {
+    if (this.props.myData.professionalExperiences.length < 1) {
+      return null;
+    }
+    return 'Experiences';
+  }
+
   render() {
     return (
       <Content>
-        <Education educations={this.props.myData.educations}/>
-        <Projects projects={this.props.myData.otherProjects}/>
-        <Experiences experiences={this.props.myData.professionalExperiences}/>
+        {this.renderEducationSection()}
+        {this.renderExperienceSection()}
+        {this.renderProjectSection()}
       </Content>
     );
   }
