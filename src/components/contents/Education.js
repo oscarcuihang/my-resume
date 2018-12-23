bb84c8c4f223006d0959d2cdc6f32f88c0ed3624
Hang Cui <oscarcuihang@hotmail.com>
2018-12-23 16:34:28 -0500

    Add I18N (#4)

--- src/components/contents/Education.js
+++ src/components/contents/Education.js
@@ -5,14 +5,18 @@ class Educations extends Component {
   static propTypes = {
     educations: PropTypes.array.isRequired,
   }
-
+  t(id) {
+    return this.context.intl.formatMessage({id: id});
+  }
   render() {
     return (
       <div>
-        Educations
+        <h2>{this.t('education.header')}</h2>
+        
       </div>
     );
   }
 }
+Educations.contextTypes ={ intl: PropTypes.object.isRequired };
 
 export default Educations;
